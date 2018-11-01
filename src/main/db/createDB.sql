45949bbe34f24c6dc522bd65dcea2ab2cc0ab572
Elan <elanbmiller@gmail.com>
2018-11-01 12:36:47 -0500

    updating db script

--- src/main/db/createDB.sql
+++ src/main/db/createDB.sql
@@ -1,26 +1,26 @@
+USE courseEvalsDB;
 drop table courses;
-create table if not exists courses (
+create table if not exists courses ( 
 	id BIGINT UNSIGNED not null auto_increment,
 	courseTitle VARCHAR(255) not null,
-	content DECIMAL(4, 2) unsigned zerofill,
-    readingsAreUseful DECIMAL(4, 2) unsigned zerofill,
-    hwsAreUseful DECIMAL(4, 2) unsigned zerofill,
-    reasonableWorkload DECIMAL(4, 2) unsigned zerofill,
-    goodProfFeedback DECIMAL(4, 2) unsigned zerofill,
-    labsAreHelpful DECIMAL(4, 2) unsigned zerofill,
-    relevant DECIMAL(4, 2) unsigned zerofill,
-    examsReflectMaterial DECIMAL(4, 2) unsigned zerofill,
-    enoughTimeForExams DECIMAL(4, 2) unsigned zerofill,
-    gradesReflectUnderstandingMaterial DECIMAL(4, 2) unsigned zerofill,
-    fairGrading DECIMAL(4, 2) unsigned zerofill,
-    hoursPerWeekSpentOnCourse DECIMAL(4, 2) unsigned zerofill,
-    percentOfLecturesAttended DECIMAL(4, 2) unsigned zerofill,
-    wouldYouRecommendCourse DECIMAL(4, 2) unsigned zerofill,
+    syllabusAccuracy DECIMAL(4, 2) unsigned zerofill, 
+    responseCount DECIMAL(4, 2) unsigned zerofill, 
+    descriptionAccuracy DECIMAL(4, 2) unsigned zerofill, 
+    profName VARCHAR(255), 
+    profQuality DECIMAL(4, 2) unsigned zerofill, 
+    courseQuality DECIMAL(4, 2) unsigned zerofill, 
+    textBook DECIMAL(4, 2) unsigned zerofill, 
+    worldApplication DECIMAL(4, 2) unsigned zerofill, 
+    examRelevance DECIMAL(4, 2) unsigned zerofill, 
+    examTime DECIMAL(4, 2) unsigned zerofill, 
+    fairGrade DECIMAL(4, 2) unsigned zerofill, 
+    gradeConsistent DECIMAL(4, 2) unsigned zerofill, 
+    gradeAggregate DECIMAL(4, 2) unsigned zerofill,
     primary key (id)
 )engine = InnoDB default character set = utf8 collate = utf8_general_ci;
-insert into courses (courseTitle, content, readingsAreUseful, hwsAreUseful, reasonableWorkload, goodProfFeedback, labsAreHelpful,
-relevant, examsReflectMaterial, enoughTimeForExams, gradesReflectUnderstandingMaterial, fairGrading, hoursPerWeekSpentOnCourse,
-percentOfLecturesAttended, wouldYouRecommendCourse)
-VALUES('CSETEST', 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5);
+insert into courses (courseTitle, syllabusAccuracy, responseCount, descriptionAccuracy, profName, profQuality, courseQuality,
+textBook, worldApplication, examRelevance, examTime, fairGrade, gradeConsistent,
+gradeAggregate)
+VALUES('CSETEST', 5.5, 5.5, 5.5, 'Name of Professor', 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5);
 
 select * from courses;
