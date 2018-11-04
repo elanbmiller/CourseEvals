4fd629f68aeebcde19cac14158ba41bbbeb6ebfe
Elan <elanbmiller@gmail.com>
2018-11-04 18:55:35 -0500

    adding login and signup pages

---
+++ src/main/testHTML/js/jsStyles.js
@@ -0,0 +1,5 @@
+$(window).scroll(function(){
+    $('nav').toggleClass('navScroll', $(this).scrollTop() > 10);
+    $('nav div ul li a.nav-link').toggleClass('navLinkScroll', $(this).scrollTop() > 10);
+});
+
