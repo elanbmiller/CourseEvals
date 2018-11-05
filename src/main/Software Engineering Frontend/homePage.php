cf3ecd9e27b87d1ec10806ae2d8355db9e010c45
Elan <elanbmiller@gmail.com>
2018-11-05 18:08:05 -0500

    completely pretty UI

--- src/main/Software Engineering Frontend/homePage.php
+++ src/main/Software Engineering Frontend/homePage.php
@@ -9,6 +9,9 @@
     <!--Bootstrap stuff-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="home.css">
+    <! -- Font awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
+        crossorigin="anonymous">
     	<!--JQuery-->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<title>Course Eval Explorer</title>
@@ -17,67 +20,74 @@
 <?php 
 include "home.php";
 ?>
-<div class="container-fluid">
+    <div class="container-fluid d-flex flex-column h-100 mt-5" id="main">
 
-<!-- Course Listings Main Page Header -->
-   <div class="row">
-      <div class="col text-center">
-         <h1 class="text-primary">Course Evaluation Explorer</h1>
-      </div>
-   </div>
-
-<!-- Table showing added courses -->
-   <div class="row">
-      <div class="col my-5">
-         <!-- comparison table -->
-         <table id="courseTable">
-            <thead>
-               <th onclick="sortAddedClasses(&quot;courseTitle&quot;)">Course Title</th>
-               <th onclick="sortAddedClasses(&quot;profName&quot;)">Professor Name</th>
-               <th onclick="sortAddedClasses(&quot;syllabusAccuracy&quot;)">Syllabus Accuracy</th>
-               <th onclick="sortAddedClasses(&quot;responseCount&quot;)">Response Count</th>
-               <th onclick="sortAddedClasses(&quot;descriptionAccuracy&quot;)">Description Accuracy</th>
-               <th onclick="sortAddedClasses(&quot;profQuality&quot;)">Professor Quality</th>
-               <th onclick="sortAddedClasses(&quot;courseQuality&quot;)">Course Quality</th>
-               <th onclick="sortAddedClasses(&quot;textbook&quot;)">Textbook</th>
-               <th onclick="sortAddedClasses(&quot;worldApplication&quot;)">World Application</th>
-               <th onclick="sortAddedClasses(&quot;examRelevance&quot;)">Exam Relevance</th>
-               <th onclick="sortAddedClasses(&quot;examTime&quot;)">Exam Time</th>
-               <th onclick="sortAddedClasses(&quot;fairGrade&quot;)">Fair Grade Earned</th>
-               <th onclick="sortAddedClasses(&quot;gradeConsistent&quot;)">Grades Consistent</th>
-               <th onclick="sortAddedClasses(&quot;gradeAggregate&quot;)">Grade Aggregate</th>
-               <th></th>
-            </thead>
-            <tbody id="courseTableBody"></tbody>
-         </table>
-      </div>
-   </div>
-
-<!-- Button to show list of courses to choose from -->
-   <div class="row">
-      <div class="col text-center mb-5">
-         <button type="button" id="addNewClass" class="btn btn-primary">Expand list</button>
-      </div>
-   </div>
-
-<!-- Search bar -->
-   <div class="row justify-content-center">
-      <div class="col-6 text-center">
-        <div id="hideTable">
-                <input type="test" class="my-1" id="searchbar" placeholder="Search for a class or professor">
-                <ul class="list-group" id="allClassList"></ul>
+        <!-- Header title -->
+        <div class="row">
+            <div class="col d-flex flex-column flex-grow">
+                <h1 class="display-4" id="loggedInHeading">
+                    Add some courses!</br>
+                    <small class="text-muted" style="font-size:.5em;">Then, click on any of the columns to sort</small>
+                </h1>
+            </div>
         </div>
-      </div>
-   </div>
 
-<!-- List of courses
-    <div class="row justify-content-center">
-      <div class="col-6 text-center" id="hideTable">
-            
-      </div>
-   </div> -->
 
-</div>
+        <!-- Table showing added courses -->
+        <div class="row">
+            <div class="col my-5">
+                <!-- comparison table -->
+                <table id="courseTable">
+                    <thead>
+                        <th onclick="sortAddedClasses(&quot;courseTitle&quot;)">Course Title</th>
+                        <th onclick="sortAddedClasses(&quot;profName&quot;)">Professor Name</th>
+                        <th onclick="sortAddedClasses(&quot;syllabusAccuracy&quot;)">Syllabus Accuracy</th>
+                        <th onclick="sortAddedClasses(&quot;responseCount&quot;)">Response Count</th>
+                        <th onclick="sortAddedClasses(&quot;descriptionAccuracy&quot;)">Description Accuracy</th>
+                        <th onclick="sortAddedClasses(&quot;profQuality&quot;)">Professor Quality</th>
+                        <th onclick="sortAddedClasses(&quot;courseQuality&quot;)">Course Quality</th>
+                        <th onclick="sortAddedClasses(&quot;textbook&quot;)">Textbook</th>
+                        <th onclick="sortAddedClasses(&quot;worldApplication&quot;)">World Application</th>
+                        <th onclick="sortAddedClasses(&quot;examRelevance&quot;)">Exam Relevance</th>
+                        <th onclick="sortAddedClasses(&quot;examTime&quot;)">Exam Time</th>
+                        <th onclick="sortAddedClasses(&quot;fairGrade&quot;)">Fair Grade Earned</th>
+                        <th onclick="sortAddedClasses(&quot;gradeConsistent&quot;)">Grades Consistent</th>
+                        <th onclick="sortAddedClasses(&quot;gradeAggregate&quot;)">Grade Aggregate</th>
+                        <th></th>
+                    </thead>
+                    <tbody id="courseTableBody"></tbody>
+                </table>
+            </div>
+        </div>
+
+        <!-- Button to show list of courses to choose from -->
+        <div class="row">
+            <div class="col text-center mb-5">
+                <button type="button" id="addNewClass" class="btn btn-primary">Add course</button>
+            </div>
+        </div>
+
+        <!-- Search bar -->
+        <div class="row justify-content-center">
+            <div class="col-6 text-center">
+                <div id="hideTable">
+                    <!-- Search form -->
+                    <div class="row">
+                    <div class="col-1 pt-2">
+                    <i class="fa fa-search" id="searchIcon"></i>
+                    </div>
+                    <div class="col-11">
+                    <input class="form-control glyphicon glyphicon-search" id="searchbar" type="text" placeholder="Search by class or professor" aria-label="Search">
+                    </div>
+                    </div>
+                    <ul class="list-group mt-5" id="allClassList"></ul>
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
 </body>
 </html>
 
---
+++ src/main/testHTML/tableTest/home.js
@@ -0,0 +1,381 @@
+
+
+var AllRowData_JS = [];
+
+var addedCourseList = [];
+var allCourseList = [];
+
+var courseObjectIdOrder = ["courseTitle", "profName", "syllabusAccuracy", "responseCount", "descriptionAccuracy", "profQuality", "courseQuality", "textBook", "worldApplication", "examRelevance", "examTime", "fairGrade", "gradeConsistent", "gradeAggregate"];
+//this is to make loops work easier, just an array of the row column id's
+//will have to be updated when we add more data
+//used in populateMainView()
+
+
+
+$(document).ready(function(){
+
+        //listener for change in search bar
+        $('#searchbar').keyup(searchFunction);
+    //listener for add new class button click
+    $('#addNewClass').click(addClass);
+
+getCourseData();
+populateMainView();
+});
+
+//in these, id has to be the unique identifier
+//will need to change first when we get the database involved
+function getCourseData() {
+
+//push all row objects to array
+for (i = 0; i < AllRowData_JS.length; i++) {
+        allCourseList.push(AllRowData_JS[i]);
+    }
+}
+
+
+/* adds courses from addedCourseList to the main view*/
+function populateMainView() {
+        emptyAddedCourseListInView();
+    //sortAddedClasses("profRating");
+    var t = $('#courseTableBody');
+for (i=0; i < addedCourseList.length; i++) { //loop through added courses
+    var tableRow = "<tr>";
+    for (j=0; j < courseObjectIdOrder.length; j++) { //add data points to new td
+            tableRow += "<td>" + addedCourseList[i][courseObjectIdOrder[j]] + "</td>";
+        }
+
+//add the remove button
+tableRow += "<td> <button onclick=\"removeAddedCourse(" + addedCourseList[i]["courseTitle"] + ")\">remove</button> </td>" //course title is acting as unique id here
+
+
+tableRow += "</tr>"
+t.append(tableRow);
+}
+
+}
+
+/*called by add class button*/
+function addClass() {
+    var addClass = $('#addNewClass'); //button
+    //making this function as a toggle
+    if ($(addClass).html() == "Hide list") {  //hide list
+        $(addClass).html("Expand list");
+        document.getElementById("hideTable").style.display = "none";
+    } else { //show classes to add
+        emptyAllCourseList();
+        document.getElementById("hideTable").style.display = "block";
+        $(addClass).html("Hide list");
+        for (i = 0; i < allCourseList.length; i++) {
+            //create list item
+            let newItem = "<li class=\"list-group-item\" onclick=\"addClassToList(" + allCourseList[i][courseObjectIdOrder[0]] + ")\" id=\"" + allCourseList[i][courseObjectIdOrder[0]] + "\"> " +
+                allCourseList[i][courseObjectIdOrder[0]] + ", " + allCourseList[i][courseObjectIdOrder[1]] + "</li>";
+            //append to ul made in the HTML
+            $("#allClassList").append(newItem);
+        }
+    }
+}
+
+function showCoursesToAdd() {
+    emptyAllCourseList();
+    for (i = 0; i < allCourseList.length; i++) {
+        //create list item
+        let newItem = "<li class=\"list-group-item\" onclick=\"addClassToList(" + allCourseList[i][courseObjectIdOrder[0]] + ")\" id=\"" + allCourseList[i][courseObjectIdOrder[0]] + "\"> " +
+            allCourseList[i][courseObjectIdOrder[0]] + ", " + allCourseList[i][courseObjectIdOrder[1]] + "</li>";
+        //append to ul made in the HTML
+        $("#allClassList").append(newItem);
+    }
+}
+
+function addClassToList(classID) {
+    var courseIdToTest = classID.innerHTML.split(",");
+    var test = courseIdToTest[0];
+    for (i = 0; i < allCourseList.length; i++) {
+        if ($.trim(String(allCourseList[i][courseObjectIdOrder[0]])) == $.trim(String(test))) {
+            addedCourseList.push(allCourseList[i]);
+            allCourseList.splice(i, 1);
+            $('#addNewClass').html("Add course");
+            $('#hideTable').hidden = true;
+            emptyAllCourseList();
+            //setup();
+            //return;
+        }
+    }
+    populateMainView();
+}
+
+function emptyAddedCourseListInView() {
+    $("#courseTableBody tr").remove();
+}
+
+function emptyAllCourseList() {
+    $("#allClassList li").remove();
+}
+
+function searchFunction() {
+    console.log("We are going to search for " + $('#searchbar').val());
+    var searchString = $('#searchbar').val();
+    for (i = 0; i < allCourseList.length; i++) { //loop through unadded courses
+        if (allCourseList[i]["courseTitle"].toLowerCase().includes(searchString.toLowerCase()) || allCourseList[i]["profName"].toLowerCase().includes(searchString.toLowerCase())) {
+            //if course title or prof name contain the search string (not case-sensitive)
+            console.log("found a match");
+            var foundCourse = allCourseList[i];
+            allCourseList.splice(i, 1); //remove foundCourse
+            allCourseList.unshift(foundCourse); //add it back at the beginning
+        }
+    }
+    console.log("finished search");
+    showCoursesToAdd();
+}
+
+function removeAddedCourse(courseTitle) {
+    console.log(courseTitle);
+    var courseToRemove = null;
+    for (i = 0; i < addedCourseList.length; i++) {
+        if ($.trim(String(addedCourseList[i]["courseTitle"])) == $.trim(String(courseTitle))) {
+            courseToRemove = addedCourseList[i];
+            addedCourseList.splice(i, 1); //remove from visible course list
+        }
+    }
+
+    //shitty error handling
+    if (courseToRemove == null) {
+        console.log("error in removeAddedCourse()");
+        return;
+    }
+
+    allCourseList.push(courseToRemove);
+    populateMainView();
+
+
+}
+
+function sortAddedClasses(idToSortBy) {
+    //I could have done a switch statement but I thought of that too late
+    if (idToSortBy == "courseTitle") {
+        addedCourseList.sort(compareCourseTitle);
+    } else if (idToSortBy == "profName") {
+        addedCourseList.sort(compareProfName);
+    } else if (idToSortBy == "syllabusAccuracy") {
+        addedCourseList.sort(compareSyllabusAccuracy)
+    } else if (idToSortBy == "responseCount") {
+        addedCourseList.sort(compareResponseCount)
+    } else if (idToSortBy == "descriptionAccuracy") {
+        addedCourseList.sort(compareDescriptionAccuracy)
+    } else if (idToSortBy == "profQuality") {
+        addedCourseList.sort(compareProfQuality)
+    } else if (idToSortBy == "courseQuality") {
+        addedCourseList.sort(comparecourseQuality)
+    } else if (idToSortBy == "textbook") {
+        addedCourseList.sort(compareTextbook)
+    } else if (idToSortBy == "worldApplication") {
+        addedCourseList.sort(compareWorldApplication)
+    } else if (idToSortBy == "examRelevance") {
+        addedCourseList.sort(compareExamRelevance)
+    } else if (idToSortBy == "examTime") {
+        addedCourseList.sort(compareExamTime)
+    } else if (idToSortBy == "fairGrade") {
+        addedCourseList.sort(compareFairGrade)
+    } else if (idToSortBy == "gradeConsistent") {
+        addedCourseList.sort(compareGradeConsistent)
+    } else if (idToSortBy == "gradeAggregate") {
+        addedCourseList.sort(compareGradeAggregate)
+    }
+    populateMainView();
+}
+
+
+/**
+
+SORTER HELP FUNCTIONS UNDER HERE
+
+**/
+
+function compareCourseTitle(a, b) {
+    const CRA = a.courseTitle;
+    const CRB = b.courseTitle;
+    let comparison = 0;
+
+    if (CRA > CRB) {
+        comparison = 1;
+    } else if (CRA < CRB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+function compareProfName(a, b) {
+    const CRA = a.profName;
+    const CRB = b.profName;
+    let comparison = 0;
+
+    if (CRA > CRB) {
+        comparison = 1;
+    } else if (CRA < CRB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+function compareSyllabusAccuracy(a, b) {
+    const CRA = a.syllabusAccuracy;
+    const CRB = b.syllabusAccuracy;
+    let comparison = 0;
+
+    if (CRA > CRB) {
+        comparison = 1;
+    } else if (CRA < CRB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+function compareResponseCount(a, b) {
+    const CRA = a.responseCount;
+    const CRB = b.responseCount;
+    let comparison = 0;
+
+    if (CRA > CRB) {
+        comparison = 1;
+    } else if (CRA < CRB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+function compareDescriptionAccuracy(a, b) {
+    const CRA = a.descriptionAccuracy;
+    const CRB = b.descriptionAccuracy;
+    let comparison = 0;
+
+    if (CRA > CRB) {
+        comparison = 1;
+    } else if (CRA < CRB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+function compareProfQuality(a, b) {
+    const CRA = a.profQuality;
+    const CRB = b.profQuality;
+    let comparison = 0;
+
+    if (CRA > CRB) {
+        comparison = 1;
+    } else if (CRA < CRB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+function comparecourseQuality(a, b) {
+    const CRA = a.courseQuality;
+    const CRB = b.courseQuality;
+    let comparison = 0;
+
+    if (CRA > CRB) {
+        comparison = 1;
+    } else if (CRA < CRB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+function compareTextbook(a, b) {
+    const CRA = a.textBook;
+    const CRB = b.textBook;
+    let comparison = 0;
+
+    if (CRA > CRB) {
+        comparison = 1;
+    } else if (CRA < CRB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+function compareWorldApplication(a, b) {
+    const CRA = a.worldApplication;
+    const CRB = b.worldApplication;
+    let comparison = 0;
+
+    if (CRA > CRB) {
+        comparison = 1;
+    } else if (CRA < CRB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+function compareExamRelevance(a, b) {
+    const CRA = a.examRelevance;
+    const CRB = b.examRelevance;
+    let comparison = 0;
+
+    if (CRA > CRB) {
+        comparison = 1;
+    } else if (CRA < CRB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+function compareExamTime(a, b) {
+    const CRA = a.examTime;
+    const CRB = b.examTime;
+    let comparison = 0;
+
+    if (CRA > CRB) {
+        comparison = 1;
+    } else if (CRA < CRB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+function compareFairGrade(a, b) {
+    const CRA = a.fairGrade;
+    const CRB = b.fairGrade;
+    let comparison = 0;
+
+    if (CRA > CRB) {
+        comparison = 1;
+    } else if (CRA < CRB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+function compareGradeConsistent(a, b) {
+    const CRA = a.gradeConsistent;
+    const CRB = b.gradeConsistent;
+    let comparison = 0;
+
+    if (CRA > CRB) {
+        comparison = 1;
+    } else if (CRA < CRB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+function compareGradeAggregate(a, b) {
+    const CRA = a.gradeAggregate;
+    const CRB = b.gradeAggregate;
+    let comparison = 0;
+
+    if (CRA > CRB) {
+        comparison = 1;
+    } else if (CRA < CRB) {
+        comparison = -1;
+    }
+    return comparison;
+}
+
+/*
+holy shit that's a lot of functions. But I'm in too deep to change it.
+*/
+
+
+
+
