c8e023cc136f050b9fa3094d042b965e324aa9f5
Elan <elanbmiller@gmail.com>
2018-11-04 21:41:19 -0500

    actually getting it to go through

--- src/main/db/createDB.sql
+++ src/main/db/createDB.sql
@@ -1,49 +1,49 @@
 USE courseEvalsDB;
-drop table courses;
-create table if not exists courses ( 
-	id BIGINT UNSIGNED not null auto_increment,
-	courseTitle VARCHAR(255) not null,
-    syllabusAccuracy DECIMAL(4, 2) unsigned zerofill, 
-    responseCount DECIMAL(4, 2) unsigned zerofill, 
-    descriptionAccuracy DECIMAL(4, 2) unsigned zerofill, 
-    profName VARCHAR(255), 
-    profQuality DECIMAL(4, 2) unsigned zerofill, 
-    courseQuality DECIMAL(4, 2) unsigned zerofill, 
-    textBook DECIMAL(4, 2) unsigned zerofill, 
-    worldApplication DECIMAL(4, 2) unsigned zerofill, 
-    examRelevance DECIMAL(4, 2) unsigned zerofill, 
-    examTime DECIMAL(4, 2) unsigned zerofill, 
-    fairGrade DECIMAL(4, 2) unsigned zerofill, 
-    gradeConsistent DECIMAL(4, 2) unsigned zerofill, 
-    gradeAggregate DECIMAL(4, 2) unsigned zerofill,
-    primary key (id)
-)engine = InnoDB default character set = utf8 collate = utf8_general_ci;
-insert into courses (courseTitle, syllabusAccuracy, responseCount, descriptionAccuracy, profName, profQuality, courseQuality,
-textBook, worldApplication, examRelevance, examTime, fairGrade, gradeConsistent,
-gradeAggregate)
-VALUES('cse131', 5.5, 5.5, 5.5, 'First Prof', 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5);
-
-insert into courses (courseTitle, syllabusAccuracy, responseCount, descriptionAccuracy, profName, profQuality, courseQuality,
-textBook, worldApplication, examRelevance, examTime, fairGrade, gradeConsistent,
-gradeAggregate)
-VALUES('fss132', 5.5, 5.5, 5.5, 'SecondProfessor', 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5);
-
-insert into courses (courseTitle, syllabusAccuracy, responseCount, descriptionAccuracy, profName, profQuality, courseQuality,
-textBook, worldApplication, examRelevance, examTime, fairGrade, gradeConsistent,
-gradeAggregate)
-VALUES('introToCs', 5.5, 5.5, 5.5, 'third prof', 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5);
-
-
-insert into courses (courseTitle, syllabusAccuracy, responseCount, descriptionAccuracy, profName, profQuality, courseQuality,
-textBook, worldApplication, examRelevance, examTime, fairGrade, gradeConsistent,
-gradeAggregate)
-VALUES('cse437', 5.5, 5.5, 5.5, 'Dan Evans', 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5);
-
-
-insert into courses (courseTitle, syllabusAccuracy, responseCount, descriptionAccuracy, profName, profQuality, courseQuality,
-textBook, worldApplication, examRelevance, examTime, fairGrade, gradeConsistent,
-gradeAggregate)
-VALUES('cse102', 5.5, 5.5, 5.5, 'Sir Walter Scott', 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5);
+-- drop table courses;
+-- create table if not exists courses ( 
+-- 	id BIGINT UNSIGNED not null auto_increment,
+-- 	courseTitle VARCHAR(255) not null,
+--     syllabusAccuracy DECIMAL(4, 2) unsigned zerofill, 
+--     responseCount DECIMAL(4, 2) unsigned zerofill, 
+--     descriptionAccuracy DECIMAL(4, 2) unsigned zerofill, 
+--     profName VARCHAR(255), 
+--     profQuality DECIMAL(4, 2) unsigned zerofill, 
+--     courseQuality DECIMAL(4, 2) unsigned zerofill, 
+--     textBook DECIMAL(4, 2) unsigned zerofill, 
+--     worldApplication DECIMAL(4, 2) unsigned zerofill, 
+--     examRelevance DECIMAL(4, 2) unsigned zerofill, 
+--     examTime DECIMAL(4, 2) unsigned zerofill, 
+--     fairGrade DECIMAL(4, 2) unsigned zerofill, 
+--     gradeConsistent DECIMAL(4, 2) unsigned zerofill, 
+--     gradeAggregate DECIMAL(4, 2) unsigned zerofill,
+--     primary key (id)
+-- )engine = InnoDB default character set = utf8 collate = utf8_general_ci;
+-- insert into courses (courseTitle, syllabusAccuracy, responseCount, descriptionAccuracy, profName, profQuality, courseQuality,
+-- textBook, worldApplication, examRelevance, examTime, fairGrade, gradeConsistent,
+-- gradeAggregate)
+-- VALUES('cse131', 5.5, 5.5, 5.5, 'First Prof', 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5);
+-- 
+-- insert into courses (courseTitle, syllabusAccuracy, responseCount, descriptionAccuracy, profName, profQuality, courseQuality,
+-- textBook, worldApplication, examRelevance, examTime, fairGrade, gradeConsistent,
+-- gradeAggregate)
+-- VALUES('fss132', 5.5, 5.5, 5.5, 'SecondProfessor', 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5);
+-- 
+-- insert into courses (courseTitle, syllabusAccuracy, responseCount, descriptionAccuracy, profName, profQuality, courseQuality,
+-- textBook, worldApplication, examRelevance, examTime, fairGrade, gradeConsistent,
+-- gradeAggregate)
+-- VALUES('introToCs', 5.5, 5.5, 5.5, 'third prof', 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5);
+-- 
+-- 
+-- insert into courses (courseTitle, syllabusAccuracy, responseCount, descriptionAccuracy, profName, profQuality, courseQuality,
+-- textBook, worldApplication, examRelevance, examTime, fairGrade, gradeConsistent,
+-- gradeAggregate)
+-- VALUES('cse437', 5.5, 5.5, 5.5, 'Dan Evans', 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5);
+-- 
+-- 
+-- insert into courses (courseTitle, syllabusAccuracy, responseCount, descriptionAccuracy, profName, profQuality, courseQuality,
+-- textBook, worldApplication, examRelevance, examTime, fairGrade, gradeConsistent,
+-- gradeAggregate)
+-- VALUES('cse102', 5.5, 5.5, 5.5, 'Sir Walter Scott', 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5);
 
 
 select * from courses;
--- src/main/testHTML/index.html
+++ src/main/testHTML/index.html
@@ -114,7 +114,7 @@
                         by title or professor, and <strong>save</strong> a list of courses you're interested in.
                     </p>
                     <a href='#createAccount' role="button" class="btn w-100 h-100 btn-circle" style="border-radius: 0% !important;">
-                        <div class="display-4" id="firstCreatAccount">
+                        <div class="display-4" id="firstCreateAccount">
                             Create An Account
                         </div>
                     </a>
@@ -138,7 +138,7 @@
                         <h5 class="card-title text-center">Create An Account</h5>
                         <form class="form-signin">
                             <div class="form-label-group">
-                                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+                                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required>
                                 <label class="text-center" for="inputEmail">Email address</label>
                             </div>
 
@@ -147,7 +147,7 @@
                                 <label class="text-center" for="inputPassword">Password</label>
                             </div>
 
-                            <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Create</button>
+                            <button class="btn btn-lg btn-primary btn-block text-uppercase" id="createAccountBtn" type="submit">Create</button>
                         </form>
                     </div>
                 </div>
@@ -164,28 +164,24 @@
 
 
     <div class="container-fluid d-flex flex-column h-100" id="logIn">
-        <!-- Header title -->
         <div class="row mt-5 justify-content-center align-self-center">
             <div class="col d-flex flex-column flex-grow p-2">
 
                 <div class="card card-signin my-5">
                     <div class="card-body">
-                        <h5 class="card-title text-center">Sign In</h5>
+                        <h5 class="card-title text-center">Log In</h5>
                         <form class="form-signin">
                             <div class="form-label-group">
-                                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-                                <label class="text-center" for="inputEmail">Email address</label>
+                                <input type="email" id="loginEmail" class="form-control" placeholder="Email address" required>
+                                <label class="text-center" for="loginEmail">Email address</label>
                             </div>
 
                             <div class="form-label-group">
-                                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-                                <label class="text-center" for="inputPassword">Password</label>
+                                <input type="password" id="loginPassword" class="form-control" placeholder="Password" required>
+                                <label class="text-center" for="loginPassword">Password</label>
                             </div>
 
-                            <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
-                            <hr class="my-4">
-                            <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign in with Google</button>
-                            <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign in with Facebook</button>
+                            <button class="btn btn-lg btn-primary btn-block text-uppercase" id="loginBtn" type="submit">Log in</button>
                         </form>
                     </div>
                 </div>
