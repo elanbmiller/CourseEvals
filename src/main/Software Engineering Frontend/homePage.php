71fcb8b14e24b92922549ebb16b5791b76cd40c1
Elan <elanbmiller@gmail.com>
2018-11-05 18:27:14 -0500

    trying to get mainpage stuff to work

--- src/main/Software Engineering Frontend/homePage.php
+++ src/main/Software Engineering Frontend/homePage.php
@@ -21,79 +21,75 @@
 include "home.php";
 ?>
     <div class="container-fluid d-flex flex-column h-100 mt-5" id="main">
-
-        <!-- Header title -->
-        <div class="row">
-            <div class="col d-flex flex-column flex-grow">
-                <h1 class="display-4" id="loggedInHeading">
-                    Add some courses!</br>
-                    <small class="text-muted" style="font-size:.5em;">Then, click on any of the columns to sort</small>
-                </h1>
-            </div>
-        </div>
-
-
-        <!-- Table showing added courses -->
-        <div class="row">
-            <div class="col my-5">
-                <!-- comparison table -->
-                <table id="courseTable">
-                    <thead>
-                        <th onclick="sortAddedClasses(&quot;courseTitle&quot;)">Course Title</th>
-                        <th onclick="sortAddedClasses(&quot;profName&quot;)">Professor Name</th>
-                        <th onclick="sortAddedClasses(&quot;syllabusAccuracy&quot;)">Syllabus Accuracy</th>
-                        <th onclick="sortAddedClasses(&quot;responseCount&quot;)">Response Count</th>
-                        <th onclick="sortAddedClasses(&quot;descriptionAccuracy&quot;)">Description Accuracy</th>
-                        <th onclick="sortAddedClasses(&quot;profQuality&quot;)">Professor Quality</th>
-                        <th onclick="sortAddedClasses(&quot;courseQuality&quot;)">Course Quality</th>
-                        <th onclick="sortAddedClasses(&quot;textbook&quot;)">Textbook</th>
-                        <th onclick="sortAddedClasses(&quot;worldApplication&quot;)">World Application</th>
-                        <th onclick="sortAddedClasses(&quot;examRelevance&quot;)">Exam Relevance</th>
-                        <th onclick="sortAddedClasses(&quot;examTime&quot;)">Exam Time</th>
-                        <th onclick="sortAddedClasses(&quot;fairGrade&quot;)">Fair Grade Earned</th>
-                        <th onclick="sortAddedClasses(&quot;gradeConsistent&quot;)">Grades Consistent</th>
-                        <th onclick="sortAddedClasses(&quot;gradeAggregate&quot;)">Grade Aggregate</th>
-                        <th></th>
-                    </thead>
-                    <tbody id="courseTableBody"></tbody>
-                </table>
-            </div>
-        </div>
-
-        <!-- spacing -->
-        <div class="row my-5"></div>
-
-        <!-- Button to show list of courses to choose from -->
-        <div class="row">
-            <div class="col text-center">
-                <button type="button" id="addNewClass" class="btn btn-primary">Add course</button>
-            </div>
-        </div>
-
-        <!-- spacing -->
-        <div class="row my-5"></div>
-
-        <!-- Search bar -->
-        <div class="row justify-content-center">
-            <div class="col-6 text-center">
-                <div id="hideTable">
-                    <!-- Search form -->
-                    <div class="row">
-                    <div class="col-1 pt-2">
-                    <i class="fa fa-search" id="searchIcon"></i>
-                    </div>
-                    <div class="col-11">
-                    <input class="form-control glyphicon glyphicon-search" id="searchbar" type="text" placeholder="Search by class or professor" aria-label="Search">
-                    </div>
-                    </div>
-                    <ul class="list-group mt-5" id="allClassList"></ul>
+    
+            <!-- Header title -->
+            <div class="row">
+                <div class="col d-flex flex-column flex-grow">
+                    <h1 class="display-4" id="loggedInHeading">
+                        Add some courses!</br>
+                        <small class="text-muted" style="font-size:.5em;">Then, click on any of the columns to sort</small>
+                    </h1>
                 </div>
             </div>
+    
+    
+            <!-- Table showing added courses -->
+            <div class="row">
+                <div class="col my-5">
+                    <!-- comparison table -->
+                    <table class="mb-2" id="courseTable">
+                        <thead>
+                            <th onclick="sortAddedClasses(&quot;courseTitle&quot;)">Course Title</th>
+                            <th onclick="sortAddedClasses(&quot;profName&quot;)">Professor Name</th>
+                            <th onclick="sortAddedClasses(&quot;syllabusAccuracy&quot;)">Syllabus Accuracy</th>
+                            <th onclick="sortAddedClasses(&quot;responseCount&quot;)">Response Count</th>
+                            <th onclick="sortAddedClasses(&quot;descriptionAccuracy&quot;)">Description Accuracy</th>
+                            <th onclick="sortAddedClasses(&quot;profQuality&quot;)">Professor Quality</th>
+                            <th onclick="sortAddedClasses(&quot;courseQuality&quot;)">Course Quality</th>
+                            <th onclick="sortAddedClasses(&quot;textbook&quot;)">Textbook</th>
+                            <th onclick="sortAddedClasses(&quot;worldApplication&quot;)">World Application</th>
+                            <th onclick="sortAddedClasses(&quot;examRelevance&quot;)">Exam Relevance</th>
+                            <th onclick="sortAddedClasses(&quot;examTime&quot;)">Exam Time</th>
+                            <th onclick="sortAddedClasses(&quot;fairGrade&quot;)">Fair Grade Earned</th>
+                            <th onclick="sortAddedClasses(&quot;gradeConsistent&quot;)">Grades Consistent</th>
+                            <th onclick="sortAddedClasses(&quot;gradeAggregate&quot;)">Grade Aggregate</th>
+                            <th></th>
+                        </thead>
+                        <tbody id="courseTableBody"></tbody>
+                    </table>
+                </div>
+            </div>
+    
+            <!-- Button to show list of courses to choose from -->
+            <div class="row mb-2">
+                <div class="col text-center">
+                    <button type="button" id="addNewClass" class="btn btn-primary">Add course</button>
+                </div>
+            </div>
+    
+    
+            <!-- Search bar -->
+            <div class="row justify-content-center mt-2">
+                <div class="col-6 text-center">
+                    <div id="hideTable">
+                        <!-- Search form -->
+                        <div class="row">
+                            <div class="col-1 pt-2">
+                                <i class="fa fa-search" id="searchIcon"></i>
+                            </div>
+                            <div class="col-11">
+                                <input class="form-control glyphicon glyphicon-search" id="searchbar" type="text" placeholder="Search by class or professor"
+                                    aria-label="Search">
+                            </div>
+                        </div>
+                        <ul class="list-group mt-5" id="allClassList"></ul>
+                    </div>
+                </div>
+            </div>
+    
+    
+    
         </div>
-
-
-
-    </div>
 </body>
 </html>
 
