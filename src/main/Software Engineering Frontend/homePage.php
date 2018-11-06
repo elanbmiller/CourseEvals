34b91da64c940c44ea1b662356ec5765aac17a73
Elan <elanbmiller@gmail.com>
2018-11-06 13:21:07 -0500

    reverting

--- src/main/Software Engineering Frontend/homePage.php
+++ src/main/Software Engineering Frontend/homePage.php
@@ -18,7 +18,7 @@
 </head>
 <body>
 <?php 
-include "/CourseEvals/src/main/indexPages/session.php";
+#include "/CourseEvals/src/main/indexPages/session.php";
 include "/CourseEvals/src/main/Software Engineering Frontend/home.php";
 ?>
     <div class="container-fluid d-flex flex-column h-100 mt-5" id="main">
